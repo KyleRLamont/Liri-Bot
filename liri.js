b9db7fcd51aa26f3ba56d6d10fedf44285d8b162
Kyle LaMont <kylerlamont@gmail.com>
2019-10-30 15:41:22 -0700

    added call function for do-what-it-says, however, currently only calls spotify

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var moment = require("moment");
 var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 
@@ -29,63 +30,49 @@ switch (command) {
         break;
 }
 
-function concertsearch() {
-    var queryURL = "https://rest.bandsintown.com/artists/" + searchterm + "/events?app_id=codingbootcamp";
+function concertsearch(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     axios.get(queryURL).then(function (response) {
         for (i = 0; i < response.data.length; i++) {
             console.log("Name of Venue: " + response.data[i].venue.name + "\nLocation: " + response.data[i].venue.city + "\n Date: " + moment(response.data[i].datetime).format('MM/DD/YYYY'))
         };
     }).catch(function (error) {
-        if (error.response) {
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
-        } else if (error.request) {
-            console.log(error.request);
-        } else {
-            console.log("Error", error.message);
-        }
-        console.log(error.config);
+        console.log(error)
     });
 };
 
-function spotifysearch() {
-    spotify
-        .search({ type: 'track', query: searchterm, limit: 1 }, function (error, data) {
-            if (!error) {
-                for (var i = 0; i < data.tracks.items.length; i++) {
-                    var songData = data.tracks.items[i];
-                    console.log("Artist: " + songData.artists[0].name + "\nSong: " + songData.name + "\nPreview URL: " + songData.preview_url + "\nAlbum: " + songData.album.name + "\n-----------------------");
-                }
-            } else {
-                console.log('Error occurred.');
+function spotifysearch(song) {
+    spotify.search({ type: 'track', query: song, limit: 1 }, function (error, data) {
+        if (!error) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                console.log("Artist: " + songData.artists[0].name + "\nSong: " + songData.name + "\nPreview URL: " + songData.preview_url + "\nAlbum: " + songData.album.name + "\n-----------------------");
             }
-        });
+        } else {
+            console.log('Error occurred.');
+        }
+    });
 };
 
-function moviesearch(searchterm) {
-    var omdbURL = "http://www.omdbapi.com/?t=" + searchterm + "&y=&plot=short&apikey=trilogy";
+function moviesearch(movie) {
+    var omdbURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
     axios.get(omdbURL).then(function (response) {
-        console.log("Title: " + response.data.Title);
-        console.log("Release Year: " + response.data.Year);
-        console.log("IMdB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-        console.log("Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-            
-
-        }).catch(function(error){
-            console.log('Error occurred.');
-        })
-    };
+        console.log("Title: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nIMdB Rating: " + response.data.imdbRating + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors);
+    }).catch(function (error) {
+        console.log(error);
+    })
+};
 
 function dowhatsays() {
+    fs.readFile('random.txt', "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
 
-};
+        var txt = data.split(',');
+        
+        spotifysearch(txt[1]);
+        }
+    )}
