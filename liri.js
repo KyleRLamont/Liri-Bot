79360640ac4146374f83e4edd56319411868e955
Kyle LaMont <kylerlamont@gmail.com>
2019-10-30 10:06:30 -0700

    added basic switch arguement for initial commands and initial functions for each command

--- liri.js
+++ liri.js
@@ -1,6 +1,28 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var axios = require("axios");
 
 var spotify = new Spotify(keys.spotify);
+
+var inputstr = process.argv;
+var command = inputstr[2];
+var searchterm = inputstr[3];
+var output;
+
+switch(command){
+    case "concert-this":
+        output = concertsearch();
+        break;
+    case "spotify-this-song":
+        output = spotifysearch();
+        break;
+    case "movie-this":
+        output = moviesearch();
+        break;
+    case "do-what-it-says":
+        output = dowhatsays();
+        break;
+}
+console.log(output);
 
